166e5c83ef77036fc81bd3e7ba647b11f8d00b0e
MadMoth125 <avery.j.sims@gmail.com>
2023-09-10 14:25:53 -0500

    Finalized project

---
+++ AnimalClasses/AnimalClasses/Animals.cs
@@ -0,0 +1,67 @@
+﻿namespace AnimalClasses
+{
+	public class Animal
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+
+		public Animal(string name, int age)
+		{
+			Name = name;
+			Age = age;
+		}
+
+		public virtual void MakeSound()
+		{
+			Console.WriteLine("MakeSound() | The animal makes a generic sound.");
+		}
+	}
+
+	// Derived Dog class
+	public class Dog : Animal
+	{
+		public Dog(string name, int age) : base(name, age) { }
+
+		public void WagTail()
+		{
+			Console.WriteLine($"WagTail() | {Name} wags its tail.");
+		}
+
+		public override void MakeSound()
+		{
+			Console.WriteLine($"MakeSound() | {Name} the dog barks.");
+		}
+	}
+
+	// Derived Cat class
+	public class Cat : Animal
+	{
+		public Cat(string name, int age) : base(name, age) { }
+
+		public void ScratchFurniture()
+		{
+			Console.WriteLine($"ScratchFurniture() | {Name} the cat scratches the furniture.");
+		}
+
+		public override void MakeSound()
+		{
+			Console.WriteLine($"MakeSound() | {Name} the cat meows.");
+		}
+	}
+
+	// Derived Bird class
+	public class Bird : Animal
+	{
+		public Bird(string name, int age) : base(name, age) { }
+
+		public void Fly()
+		{
+			Console.WriteLine($"Fly() | {Name} the bird flies.");
+		}
+
+		public override void MakeSound()
+		{
+			Console.WriteLine($"MakeSound() | {Name} the bird chirps.");
+		}
+	}
+}
--- AnimalClasses/AnimalClasses/Program.cs
+++ AnimalClasses/AnimalClasses/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace AnimalClasses
+﻿using CustomConsole;
+
+namespace AnimalClasses
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		// the Main method has been made async to allow for the use of Task.Delay()
+		static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			float delayTime = 0.5f;
+			
+			List<Animal> animals = new List<Animal>
+			{
+				new Dog("Rover", 3),
+				new Cat("Whiskers", 4),
+				new Bird("Tweetie", 2)
+			};
+
+			while (true)
+			{
+				// clear screen before printing new info
+				Console.Clear();
+
+				foreach (var animal in animals)
+				{
+					Console.WriteLine($"{animal.Name} is {animal.Age} years old.");
+
+					// wait .5 seconds before making sound
+					await Task.Delay(SecondsToMiliseconds(delayTime));
+
+					animal.MakeSound();
+
+					// wait .5 seconds before performing action
+					await Task.Delay(SecondsToMiliseconds(delayTime));
+
+					if (animal is Dog) { ((Dog)animal).WagTail(); }
+
+					if (animal is Cat) { ((Cat)animal).ScratchFurniture(); }
+					
+					if (animal is Bird) { ((Bird)animal).Fly(); }
+
+					// wait .5 seconds before continuing loop
+					await Task.Delay(SecondsToMiliseconds(delayTime));
+
+					// print blank line between animals
+					ConsoleHelper.PrintBlank();
+				}
+
+				ConsoleHelper.HaltProgram("Press any key to restart the sequence...");
+			}
+		}
+
+		private static int SecondsToMiliseconds(float seconds)
+		{
+			return (int)(seconds * 1000);
 		}
 	}
 }
